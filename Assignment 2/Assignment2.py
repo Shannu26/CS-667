6669373dd8adf0f98600d65ab3bae2f61c54d6a9
Sasank Tadepalli <sasank.tadepalli@icloud.com>
2023-02-07 21:21:26 -0800

    Updated Assignment 2

--- Assignment 2/Assignment2.py
+++ Assignment 2/Assignment2.py
@@ -1,11 +1,12 @@
 import random
+import csv
 
 population = []
 
 def generatePopulation():
 	while len(population) != 1000:
 		chromosome = []
-		genes = set([i for i in range(1, 20)])
+		genes = set([i for i in range(1, 27)])
 		while genes:
 			gene = random.choice(list(genes))
 			chromosome.append(str(gene))
@@ -14,4 +15,16 @@ def generatePopulation():
 		if chromosome not in population: population.append(chromosome)
 
 generatePopulation()
-print(population)
+# print(population)
+
+adjacencyMatrix = [[0] * 27 for _ in range(27)]
+adjacencyMatrix[0][0] = 1
+# print(adjacencyMatrix)
+
+with open('input.csv', mode ='r') as inputFile:
+	inputData = csv.reader(inputFile)
+	i = 0
+	for line in inputData:
+		if i == 0:
+			
+		print(lines, len(lines))
