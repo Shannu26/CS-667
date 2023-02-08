de3a45d58966f451a483ee44cdfef0a2ab50b102
Sasank Tadepalli <sasank.tadepalli@icloud.com>
2023-02-07 17:08:06 -0800

    Completed Population Generation

--- Assignment 2/Assignment2.py
+++ Assignment 2/Assignment2.py
@@ -1,37 +1,17 @@
-import itertools
 import random
-
-# population = list(itertools.permutations([i for i in range(1, 20)]))
-# print(population)
-# print()
-# print()
-# population = random.sample(population, 1000)
-# print(population)
 
 population = []
 
-# def generatePopulation():
-# 	queue = [[]]
-# 	while len(population) != 1000:
-# 		sample = queue.pop()
-# 		if len(sample) == 19: 
-# 			population.append(" ".join([str(num) for num in sample]))
-# 		else:
-# 			for i in range(1, 20):
-# 				if i not in sample: queue.append(sample + [i])
-
 def generatePopulation():
-	queue = [[]]
 	while len(population) != 1000:
-		sample = queue.pop()
-		if len(sample) == 19: 
-			population.append(" ".join([str(num) for num in sample]))
-		else:
-			genes = set([i for i in range(1, 20)]) - set(sample)
-			while genes:
-				num = random.choice(list(genes))
-				queue.append(sample + [num])
-				genes.remove(num)
+		chromosome = []
+		genes = set([i for i in range(1, 20)])
+		while genes:
+			gene = random.choice(list(genes))
+			chromosome.append(str(gene))
+			genes.remove(gene)
+		chromosome = " ".join(chromosome)
+		if chromosome not in population: population.append(chromosome)
 
 generatePopulation()
-print(population)	
+print(population)
