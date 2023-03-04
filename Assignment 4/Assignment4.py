b253343e3858d8451c58ba980d2dd01568c70160
Sasank Tadepalli <sasank.tadepalli@icloud.com>
2023-03-03 17:04:34 -0800

    Completed Basic Implementation

--- Assignment 4/Assignment4.py
+++ Assignment 4/Assignment4.py
@@ -47,7 +47,6 @@ def performTour(distanceMatrix, visibilityMatrix, pheromoneMatrix, startingCity,
 		# print()
 		tour.append(nextCity)
 
-	print(tour)
 	return tour
 
 def calculateTourLength(tour, distanceMatrix):
@@ -62,28 +61,80 @@ def addPheromoneTrace(tour, tourLength, pheromoneMatrix, Q):
 		pheromoneMatrix[tour[i]][tour[i + 1]] += Q / tourLength
 	pheromoneMatrix[tour[-1]][tour[0]] += Q / tourLength
 
-def implementAntColonyOptimizationAlgorithm(numberOfAnts, alpha, beta, Q):
+def performPheromoneEvaporation(pheromoneMatrix, rho):
+	for row in range(27):
+		for col in range(27):
+			pheromoneMatrix[row][col] = rho * pheromoneMatrix[row][col]
+
+def findLeastTour(antTours):
+	minTourCost = math.inf
+	minTour = None
+
+	for tour in antTours:
+		if tour[1] < minTourCost:
+			minTourCost = tour[1]
+			minTour = tour
+
+	return minTour
+
+def printLeastTour(tour, tourLength, numberToCityMapping):
+	string = "-" * 215
+	string += "\n"
+	string += "The minimum cost to perform the task is " + str(tourLength) + "\n"
+	string += "The Minimum Cost Path: " + "\n"
+	for city in tour:
+		string += numberToCityMapping[city] + "  ->  "
+	string += numberToCityMapping[tour[0]]
+	string += "\n"
+	string += "-" * 215
+	string += "\n"
+
+	return string
+
+def writeOutputDataToFile(outputData):
+	with open('output.txt', mode ='w') as outputFile:
+		outputFile.writelines(outputData)
+
+def implementAntColonyOptimizationAlgorithm(numberOfAnts, numberOfIterations, alpha, beta, rho, Q):
 	distanceMatrix, numberToCityMapping = getDistanceMatrixFromFile()
 	visibilityMatrix = [[math.inf] * 27 for _ in range(27)]
 	pheromoneMatrix = [[alpha] * 27 for _ in range(27)]
+	outputData = []
+	outputData.append("-" * 215 + "\n")
 	for row in range(27):
 		for col in range(27):
 			if row != col:
 				visibilityMatrix[row][col] = 1 / distanceMatrix[row][col]
 
-	antTours = []
-	for antNumber in range(numberOfAnts):
-		startingCity = random.randint(0, 26)
-		tour = performTour(distanceMatrix, visibilityMatrix, pheromoneMatrix, startingCity, beta)
-		tourLength = calculateTourLength(tour, distanceMatrix)
-		print(tourLength)
+	for i in range(numberOfIterations):
 		# print(pheromoneMatrix)
-		# addPheromoneTrace(tour, tourLength, pheromoneMatrix, Q)
+		antTours = []
+		for antNumber in range(numberOfAnts):
+			startingCity = random.randint(0, 26)
+			tour = performTour(distanceMatrix, visibilityMatrix, pheromoneMatrix, startingCity, beta)
+			tourLength = calculateTourLength(tour, distanceMatrix)
+			antTours.append([tour, tourLength])
+
+		minTour, minTourCost = findLeastTour(antTours)
+		# print(minTour, minTourCost)
+		outputData.append("Iteration " + str(i + 1) + "\n")
+		outputData.append(printLeastTour(minTour, minTourCost, numberToCityMapping))
 		# print(pheromoneMatrix)
+		performPheromoneEvaporation(pheromoneMatrix, rho)
+		for tour, tourLength in antTours:
+			# print(tour)
+			# print(tourLength)
+			# print(pheromoneMatrix)
+			addPheromoneTrace(tour, tourLength, pheromoneMatrix, Q)
+			# print(pheromoneMatrix)
+
+	writeOutputDataToFile(outputData)
 
 if __name__ == "__main__":
 	numberOfAnts = 27
+	numberOfIterations = 1000
 	alpha = 10
 	beta = 2
-	Q = 2
-	implementAntColonyOptimizationAlgorithm(numberOfAnts, alpha, beta, Q)
+	rho = 0.5
+	Q = 10000
+	implementAntColonyOptimizationAlgorithm(numberOfAnts, numberOfIterations, alpha, beta, rho, Q)
